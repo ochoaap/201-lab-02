53841b1cc1b90465d780d67155484e63af9c96fd
Anthony Ochoa <ochoaap@yahoo.com>
2020-10-10 18:11:21 -0700

    Completed Lab, removing console logs

--- js/app.js
+++ js/app.js
@@ -3,95 +3,81 @@
 alert('Welcome to my page! I am going to ask you a couple questions.');
 
 var userName = prompt('What is your name?');
-//console.log('Hi ' + name);
 alert('it\'s nice to meet you ' + userName);
 
 var correct = 0;
 
-// //question 1
 function questionOne() {
   var home = prompt(userName + ' am I from Washington State?').toUpperCase();
 
   if (home === 'YES' || home === 'Y') {
-    //console.log('No, I am not from Washington.');
     alert('No, I am not from Washington.');
   }
   if (home === 'NO' || home === 'N') {
-    //console.log('You are correct! I am actualy from Kansas!')
     alert('You are correct! I am actualy from Kansas!');
     correct++;
   }
 }
 questionOne();
 
-// //question 2
 function questionTwo() {
   var child = prompt(userName + ' do I have a daughter?').toUpperCase();
 
   if (child === 'YES' || child === 'Y') {
-    //console.log('Yes, I do have a daughter her name is AInsley');
     alert('Yes, I do have a daughter, her name is AInsley.');
     correct++;
   }
   if (child === 'NO' || child === 'N') {
-    //console.log('Sorry, that is not correct')
     alert('Sorry, that is not correct. I do have a daughter, and her name is AInsley.');
   }
 }
 questionTwo();
 
-// //question 3
 function questionThree() {
 
   var military = prompt(userName + ' did I serve in the military?').toUpperCase();
 
   if (military === 'YES' || military === 'Y') {
-    //console.log('Yes, I was in the United States Navy for 5 years.');
     alert('Yes, I was in the US Navy for 5 years.');
     correct++;
   }
   if (military === 'NO' || military === 'N') {
-    //console.log('Sorry, that is not correct')
     alert('Sorry, that is not correct. I served 5 years in the US Navy.');
   }
 }
 questionThree();
 
-// //question 4
+
 function questionFour() {
 
   var pets = prompt(userName + ' do I have 7 cats?').toUpperCase();
 
   if (pets === 'YES' || pets === 'Y') {
-    //console.log('No,  haha, I do not. I actually have 2 cats and 1 dog.');
     alert('No,  haha, I do not. I actually have 2 cats and 1 dog.');
   }
   if (pets === 'NO' || pets === 'N') {
-    //console.log('Correct!, I only have 2 and a dog!')
     alert('Correct!, I only have 2 and a dog!');
     correct++;
   }
 }
 questionFour();
 
-// //question 5
+
 function questionFive() {
 
   var lightsaber = prompt(userName + ' have I ever fought in a lightsaber club?').toUpperCase();
 
   if (lightsaber === 'YES' || lightsaber === 'Y') {
-    console.log('Yes! I actually have and it was a lot of fun. May the Force be with you!');
     alert('Yes! I actually have and it was a lot of fun. May the Force be with you!');
     correct++;
   }
   if (lightsaber === 'NO' || lightsaber === 'N') {
-    console.log('The Force is not with you today, I did in fact fight in a lightsaber club.');
     alert('The Force is not with you today, I did in fact fight in a lightsaber club.');
   }
 }
 questionFive();
 
-// Question 6
+
 function questionSix() {
 
   for (var i = 0; i < 4; i++) {
@@ -117,7 +103,6 @@ function questionSix() {
 }
 questionSix();
 
-// question 7
 function questionSeven() {
 
   var colors = ['blue', 'purple', 'red', 'green'];
@@ -137,11 +122,11 @@ function questionSeven() {
     }
     totalCount += 1;
   }
-    if (totalCount < 8) {
-      alert('The correct answers were blue, red, green, or purple!');
-    }
+  if (totalCount < 8) {
+    alert('The correct answers were blue, red, green, or purple!');
   }
-  
+}
+
 questionSeven();
 
 
